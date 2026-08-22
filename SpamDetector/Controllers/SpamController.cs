5a2af4308c83afbc0d2ef7f95069a5e818686eaa
mtwnicolai <twnicolai09@gmail.com>
2026-08-22 15:28:49 -0300

    feat: adiciona endpoint da analise de spam

---
+++ SpamDetector/Controllers/SpamController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpamDetector.Models;
+
+namespace SpamDetector.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SpamController : ControllerBase
+    {
+        [HttpPost("analyze")]
+        public ActionResult<SpamResponse> Analyze(SpamRequest request)
+        {
+            var response = new SpamResponse();
+            {
+                bool IsSpam = false;
+                double Probability = 0.0;
+            };
+            return Ok(response);
+        }
+    }
+}
